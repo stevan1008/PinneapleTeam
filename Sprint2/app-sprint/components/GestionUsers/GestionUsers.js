6be000e36c174d30affb5d5dc896e18ec28d84e4
stevan1008 <stevan1008@hotmail.com>
2021-10-01 12:21:34 -0500

    Creacion react-app

---
+++ Sprint2/app-sprint/components/GestionUsers/GestionUsers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './GestionUsers.module.css';
+
+const GestionUsers = () => (
+  <div className={styles.GestionUsers} data-testid="GestionUsers">
+    GestionUsers Component
+  </div>
+);
+
+GestionUsers.propTypes = {};
+
+GestionUsers.defaultProps = {};
+
+export default GestionUsers;
