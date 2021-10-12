9ff64c7ab9436c50096480ed9ec28fe29f7f3d1c
Felipe Rojas <piperojas0618@gmail.com>
2021-10-12 14:34:31 -0500

    Login Update

--- Sprint2/app-sprint/src/components/Login/Login.js
+++ Sprint2/app-sprint/src/components/Login/Login.js
@@ -1,5 +1,7 @@
 import React, { Component, useState } from 'react';
 import {FormGroup, Form, Button} from "reactstrap";
+import validator from 'validator';
+
 
 
 class Login extends Component{
@@ -10,8 +12,11 @@ class Login extends Component{
       email:'',
       password:''
     }
-
+    
+    this.setErrorMessage = '';
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    
   }
 
 
@@ -19,6 +24,20 @@ class Login extends Component{
     e.preventDefault();
   }
 
+  validate_pass = (value) => {
+    if (validator.isStrongPassword(value, {
+      minLength: 8, minLowercase: 1,
+      minUppercase: 1, minNumbers: 1, minSymbols: 1
+    })) {
+      this.setErrorMessage('Is Strong Password')
+    } else {
+      this.setErrorMessage('Is Not Strong Password')
+    }
+  }
+
+  handleChange(event) {
+    this.setState({email: event.target.email, password: event.target.password})
+  }
 
   render () {
     return (
@@ -29,9 +48,8 @@ class Login extends Component{
             <input 
               className = 'form-group'
               type="email"
-              value={this.state.email}
+              //value={this.state.email}
               onChange={(e,newValue) => this.setState({email:newValue})}
-              required
               
             />
           </FormGroup>
@@ -40,9 +58,8 @@ class Login extends Component{
             <input
               className = 'form-group'
               type="password"
-              value={this.state.password}
-              onChange={(e, newValue) => this.setState({password:newValue})}
-              required
+              //value={this.state.password}
+              onChange= {(e) => this.validate_pass(this.setState({password:e.target.password}))}
             />
           </FormGroup>
           <Button block size="lg" type="submit" color='success' >
