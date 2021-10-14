fafe7034241358ae36bdb834a1ec521b49cdd512
Felipe Rojas <piperojas0618@gmail.com>
2021-10-14 12:48:25 -0500

    Update Login.js

--- Sprint2/app-sprint/src/components/Login/Login.js
+++ Sprint2/app-sprint/src/components/Login/Login.js
@@ -2,74 +2,122 @@ import React, { Component, useState } from 'react';
 import {FormGroup, Form, Button} from "reactstrap";
 import validator from 'validator';
 
-
-
-class Login extends Component{
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      email:'',
-      password:''
-    }
-    
-    this.setErrorMessage = '';
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    
-  }
-
-
-  handleSubmit(e) {
-    e.preventDefault();
-  }
-
-  validate_pass = (value) => {
+const Login = () => {
+  
+  const [errorMessage, setErrorMessage] = useState('')
+  const [email, setEmail] = useState("");
+  const validate = (value) => {
+  
     if (validator.isStrongPassword(value, {
       minLength: 8, minLowercase: 1,
       minUppercase: 1, minNumbers: 1, minSymbols: 1
     })) {
-      this.setErrorMessage('Is Strong Password')
+      setErrorMessage('Is Strong Password')
     } else {
-      this.setErrorMessage('Is Not Strong Password')
+      setErrorMessage('Is Not Strong Password. Need to have at least one CAP, one number and a special character')
     }
   }
-
-  handleChange(event) {
-    this.setState({email: event.target.email, password: event.target.password})
+  
+  function handleSubmit(event) {
+    event.preventDefault();
   }
 
-  render () {
-    return (
-      <div className="Login">
-        <form onSubmit={this.handleSubmit}>
+  return (
+    <div style={{
+      marginLeft: '200px',
+    }}>
+    <div className="Login">
+      <pre>
+        <h2>LOGIN</h2>
+        <form onSubmit={handleSubmit}>
           <FormGroup size="lg">
-            <label>Email</label>
-            <input 
+               <label>Email</label>
+               <input 
               className = 'form-group'
               type="email"
-              //value={this.state.email}
-              onChange={(e,newValue) => this.setState({email:newValue})}
-              
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}              
             />
           </FormGroup>
-          <FormGroup size="lg">
-            <label>Password</label>
-            <input
-              className = 'form-group'
-              type="password"
-              //value={this.state.password}
-              onChange= {(e) => this.validate_pass(this.setState({password:e.target.password}))}
-            />
-          </FormGroup>
-          <Button block size="lg" type="submit" color='success' >
-            Login
-          </Button>
         </form>
-      </div>
-    );
-  }
+        <span>Enter Password: </span><input type="password"
+          onChange={(e) => validate(e.target.value)}></input> <br />
+        <span style={{
+          fontWeight: 'bold',
+          color: 'red',
+        }}>{errorMessage}</span>
+      </pre>
+    </div>
+    </div>
+  );
 }
+
+// class Login extends Component{
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email:'',
+//       password:''
+//     }
+    
+//     this.setErrorMessage = '';
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+    
+//   }
+
+
+//   handleSubmit(e) {
+//     e.preventDefault();
+//   }
+
+//   validate_pass = (value) => {
+//     if (validator.isStrongPassword(value, {
+//       minLength: 8, minLowercase: 1,
+//       minUppercase: 1, minNumbers: 1, minSymbols: 1
+//     })) {
+//       this.setErrorMessage('Is Strong Password')
+//     } else {
+//       this.setErrorMessage('Is Not Strong Password')
+//     }
+//   }
+
+//   handleChange(event) {
+//     this.setState({email: event.target.email, password: event.target.password})
+//   }
+
+//   render () {
+//     return (
+//       <div className="Login">
+//         <form onSubmit={this.handleSubmit}>
+//           <FormGroup size="lg">
+//             <label>Email</label>
+//             <input 
+//               className = 'form-group'
+//               type="email"
+//               //value={this.state.email}
+//               onChange={(e,newValue) => this.setState({email:newValue})}
+              
+//             />
+//           </FormGroup>
+//           <FormGroup size="lg">
+//             <label>Password</label>
+//             <input
+//               className = 'form-group'
+//               type="password"
+//               //value={this.state.password}
+//               onChange= {(e) => this.validate_pass(this.setState({password:e.target.password}))}
+//             />
+//           </FormGroup>
+//           <Button block size="lg" type="submit" color='success' >
+//             Login
+//           </Button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
 //onClick = {(e) => this.handleClick(e)} 
 export default Login;
